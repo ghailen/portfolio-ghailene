3f17ec2e70187125fb84d8f3b3eb7d728a7448bf
ghailen <ghailenemark@gmail.com>
2025-04-06 13:25:10 +0000

    Deploying to gh-pages from @ ghailen/portfolio-ghailene@69f9a227baee219f604b81094ae1b92a7c0d9fae 🚀

--- src/app/components/skills/skills.component.ts
+++
@@ -1,3 +0,0 @@
-import { Component } from '@angular/core';
-@Component({selector: 'app-skills',templateUrl: './skills.component.html'})
-export class SkillsComponent { skills = ['Java', 'Spring Boot', 'Angular', 'SQL', 'Docker']; }
